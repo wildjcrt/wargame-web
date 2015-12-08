b2c1fe02b54fbcd0cfa241453e2c279bdf8b9848
wildjcrt <wildjcrt@yahoo.com.tw>
2015-12-09 00:34:29 +0800

    Store counter info in window.counters and move them to its position.

--- js/config.js
+++ js/config.js
@@ -12,3 +12,13 @@ var urlParams;
     urlParams[decode(match[1])] = decode(match[2]);
 })();
 
+// Move counters according to query string
+var counters = {};
+if (urlParams.counter !== undefined) {
+  counter_array = urlParams.counter.split(',');
+  for (var i = 0; i < counter_array.length; i++) {
+    item_array = counter_array[i].split(':'); // triple items: serial, left, top
+    $('#counter' + item_array[0]).css({left: item_array[1] + 'px', top: item_array[2] + 'px'});
+    counters[item_array[0]] = item_array[1] + ':' + item_array[2];
+  }
+}
