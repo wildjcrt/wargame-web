4d50d46d62f8cbb56f46437a7e9bb5c0825d8b45
wildjcrt <wildjcrt@yahoo.com.tw>
2015-12-29 09:10:38 +0800

    Add dblclick and shuffle on .card dom in core.js

--- js/core.js
+++ js/core.js
@@ -9,10 +9,25 @@ $(document).ready(function() {
       }
     });
 
-  $('.card').draggable({delay: 200});
+  $('.card')
+    .draggable({delay: 200})
+    .on('dblclick', function() {
+      if ($(this).attr('class').indexOf('front') > 0) {
+        $(this).removeClass('front').addClass('back');
+      } else {
+        $(this).removeClass('back').addClass('front');
+      }
+    });
+
   $('.dice').on('click', function() {
     $(this).effect('highlight', {color: 'cyan'}, 'fast');
     var number = Math.floor(Math.random() * 6) + 1;
     $(this).attr('id', 'D'+number);
   });
+
+  // shuffle cards
+  var cards = $('.card.back').shuffle();
+  for (var i = 0; i < cards.length; i++) {
+    $(cards[i]).css('z-index', i+1);
+  }
 });
